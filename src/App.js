aa9b05737460f7958108155479f7240005cb87cb
Danish Shafi <danish.shafi@live.co.uk>
2022-05-24 02:24:35 +0100

    Ability to add reminders and remove task from UI added to program

--- src/App.js
+++ src/App.js
@@ -24,13 +24,26 @@ function App() {
       day: 'Feb 5th at 3:30PM',
       reminder: false,
     }
-  ]
-  )
+  ])
+
+  const deleteTask = id => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = id => {
+    setTasks(
+      tasks.map((task) =>
+       task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
 
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks'
+      }
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { FaTimes, FaRegBell } from 'react-icons/fa'
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
-      <h4>{task.text}</h4>
+    <div className={`task ${task.reminder ? 'reminder' : ''}`}>
+      <h4>
+        {task.text}
+        <div className="task-icons">
+          <FaTimes style={{color: 'red'}}
+            onClick={() => onDelete(task.id)} />
+          <FaRegBell onClick={() => onToggle(task.id)} />
+        </div>
+      </h4>
       <p>{task.day}</p>
     </div>
   )
